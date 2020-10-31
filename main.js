313742ab7c5198b1e1aa35fde44d146be8be9f8b
Riyan Dhiman <Riyandhiman14@gmail.com>
2020-10-31 16:37:01 +0530

    New Text Editor

--- main.js
+++ main.js
@@ -8,27 +8,25 @@ var fs = require('fs')
 var app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
 
 
 //To Create Server
 var server = app.listen(process.env.PORT||5000, ()=>{
-	console.log("Server Listening at http://localhost:5000");
+	console.log(`Server Listening at http://localhost:${process.env.PORT||5000}`);
 	});
 
 
-//To render HomePage i.e., to enter username
-app.get('/', (req, res, next)=>{
-	res.render('home.ejs');
-});
+// Static files
+app.use(express.static('frontend/build'));
 
+// Server React Client
+app.get("/", function(req, res) {
+	res.sendFile(path.join(__dirname, 'frontend/build/index.html'));
+});
 
 //To Get Code and Language
 app.post('/submit',async(req, res, next)=>{
